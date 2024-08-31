645d89d0f902a47ef97b99a2d00041dbdf0a0fff
arhamsaqib12 <137510400+arhamsaqib12@users.noreply.github.com>
2024-08-31 12:13:23 +0500

    Update Edit.jsx

--- src/Pages/Edit.jsx
+++ src/Pages/Edit.jsx
@@ -65,7 +65,7 @@ function EditModal({ show, handleClose }) {
       headerPic: formData.headerPic
     };
 
-    fetch(`https://twit-backend-production.up.railway.app//api/posts/user/edit/${userId}`, {
+    fetch(`https://twit-backend-production.up.railway.app/api/posts/user/edit/${userId}`, {
       method: 'PUT',
       headers: {
         'Authorization': `${localStorage.getItem('token')}`,
